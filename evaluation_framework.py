342a2839099d84e9a5ef3eeb5792bfdd4c8e73f3
mcrensh <mcrenshaw@mitre.org>
2021-10-11 01:38:24 -0600

    cleanup, command line input

--- evaluation_framework.py
+++ evaluation_framework.py
@@ -1,51 +1,77 @@
-import os
+#############################################################################
+# NOTICE                                                                    #
+#                                                                           #
+# This software (or technical data) was produced for the U.S. Government    #
+# under contract, and is subject to the Rights in Data-General Clause       #
+# 52.227-14, Alt. IV (DEC 2007).                                            #
+#                                                                           #
+# Copyright 2021 The MITRE Corporation. All Rights Reserved.                #
+#############################################################################
+
+#############################################################################
+# Copyright 2021 The MITRE Corporation                                      #
+#                                                                           #
+# Licensed under the Apache License, Version 2.0 (the "License");           #
+# you may not use this file except in compliance with the License.          #
+# You may obtain a copy of the License at                                   #
+#                                                                           #
+#    http://www.apache.org/licenses/LICENSE-2.0                             #
+#                                                                           #
+# Unless required by applicable law or agreed to in writing, software       #
+# distributed under the License is distributed on an "AS IS" BASIS,         #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+# See the License for the specific language governing permissions and       #
+# limitations under the License.                                            #
+#############################################################################
+
 import json
 import subprocess
 import shlex
-import time
+from datetime import datetime
+import sys
 from typing import Dict, Any, List
 
 
-def get_test_media(file_name: str) -> str:
-    return os.path.join(os.path.dirname(__file__), 'data', file_name)
+def main():
+    _, container_image_name, media_file = sys.argv
+
+    detection_type = "FACE"
+
+    container_id = set_up_container(container_image_name)
+
+    start_time = datetime.now()
+    output_obj = run_cli_runner_stdin_media(container_id, media_file, '-t', 'image', '-')
+    end_time = datetime.now()
+
+    tracks = get_image_tracks(output_obj, detection_type)
+
+    shut_down_container(container_id)
+
+    print("\nFound detections: ", len(tracks))
+    print("Run time: ", str(end_time - start_time))
 
 
-def _get_full_image_name(image_name):
-    test_registry = os.getenv('TEST_REGISTRY', '')
-    if len(test_registry) > 0 and test_registry[-1] != '/':
-        test_registry += '/'
-
-    test_img_tag = os.getenv('TEST_IMG_TAG')
-    if not test_img_tag:
-        test_img_tag = 'latest'
-    if test_img_tag[0] == ':':
-        test_img_tag = test_img_tag[1:]
-
-    return f'{test_registry}{image_name}:{test_img_tag}'
-
-
-def set_up_container(image_name):
-    full_image_name = _get_full_image_name(image_name)
+def set_up_container(full_image_name):
     command = ('docker', 'run', '--rm', '-d', full_image_name, '-d')
     print('Starting test container with command: ', shlex.join(command))
     proc = subprocess.run(command, stdout=subprocess.PIPE, text=True, check=True)
     return proc.stdout.strip()
 
 
-def shut_down_container(_container_id):
-    command = ('docker', 'stop', _container_id)
+def shut_down_container(container_id):
+    command = ('docker', 'stop', container_id)
     print('Stopping test container with command: ', shlex.join(command))
     subprocess.run(command, check=True)
 
 
-def run_cli_runner_stdin_media(_container_id, media_path: str, *runner_args: str) -> Dict[str, Any]:
-    return run_cli_runner_stdin_media_and_env_vars(_container_id, media_path, {}, *runner_args)
+def run_cli_runner_stdin_media(container_id, media_path: str, *runner_args: str) -> Dict[str, Any]:
+    return run_cli_runner_stdin_media_and_env_vars(container_id, media_path, {}, *runner_args)
 
 
-def run_cli_runner_stdin_media_and_env_vars(_container_id, media_path: str, env_dict: Dict[str, str],
+def run_cli_runner_stdin_media_and_env_vars(container_id, media_path: str, env_dict: Dict[str, str],
                                             *runner_args: str) -> Dict[str, Any]:
     env_params = (f'-e{k}={v}' for k, v in env_dict.items())
-    command = ['docker', 'exec', '-i', *env_params, _container_id, 'runner', *runner_args]
+    command = ['docker', 'exec', '-i', *env_params, container_id, 'runner', *runner_args]
     print('Running job with command: ', shlex.join(command))
 
     with open(media_path) as media_file, \
@@ -55,46 +81,15 @@ def run_cli_runner_stdin_media_and_env_vars(_container_id, media_path: str, env_
 
 
 def get_image_tracks(
-        expected_path,
-        expected_mime_type: str,
-        expected_job_props: Dict[str, str],
-        expected_media_metadata: Dict[str, str],
-        _output_object: Dict[str, Any],
-        _detection_type) -> List[Dict[str, Any]]:
+        output_object: Dict[str, Any],
+        detection_type) -> List[Dict[str, Any]]:
 
-    return _get_tracks(_output_object, _detection_type)
+    return get_tracks(output_object, detection_type)
 
 
-def _get_tracks(_output_object: Dict[str, Any], _detection_type) -> List[Dict[str, Any]]:
-    return _output_object['media'][0]['output'][_detection_type][0]['tracks']
+def get_tracks(output_object: Dict[str, Any], detection_type) -> List[Dict[str, Any]]:
+    return output_object['media'][0]['output'][detection_type][0]['tracks']
 
 
 if __name__ == "__main__":
-
-    start_time = time.time()
-
-    container_image_name = 'openmpf_ocv_face_detection'
-
-    _face_image = get_test_media('meds-af-S419-01_40deg.jpg')
-    _default_job_properties = {'MAX_FEATURE': '250',
-                               'MAX_OPTICAL_FLOW_ERROR': '4.7',
-                               'MIN_FACE_SIZE': '48',
-                               'MIN_INITIAL_CONFIDENCE': '10',
-                               'MIN_INIT_POINT_COUNT': '45',
-                               'MIN_POINT_PERCENT': '0.70',
-                               'VERBOSE': '0'}
-
-    detection_type = "FACE"
-
-    container_id = set_up_container(container_image_name)
-
-    output_object = run_cli_runner_stdin_media(container_id, _face_image, '-t', 'image', '-')
-    tracks = get_image_tracks(
-        '/dev/stdin', 'image/octet-stream', _default_job_properties, {}, output_object, detection_type)
-
-    shut_down_container(container_id)
-
-    end_time = time.time()
-
-    print("\nFound detections: ", len(tracks))
-    print("Run time: ", end_time - start_time)
+    main()
